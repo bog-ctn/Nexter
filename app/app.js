721a371ee462e9d0cdea00e4051baf720a636789
bog-ctn <artcorhor@gmail.com>
2023-01-25 01:35:34 +0200

    app/js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const btnHeader = document.querySelector(".header__btn");
+btnHeader.addEventListener("click", function () {
+  document
+    .querySelector(".homes")
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+});
+
+const tl = gsap.timeline({
+  defaults: { duration: 0.75, ease: "power2.Out" },
+});
+
+tl.fromTo(
+  ".span-h1",
+  { opacity: 0, x: "-100%" },
+  { opacity: 1, x: 0, duration: 1.5 }
+);
+
+tl.fromTo(
+  ".header__btn",
+  { y: "20%", opacity: 0 },
+  { y: 0, opacity: 1 },
+  "<50%"
+);
+
+// ------- Animations
+
+//--->Header
