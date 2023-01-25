ea02f93301fb04067be2e3d3bcfe68ee13dff0e3
bog-ctn <artcorhor@gmail.com>
2023-01-25 02:24:51 +0200

    up/25

--- app/app.js
+++ app/app.js
@@ -12,15 +12,18 @@ const tl = gsap.timeline({
   defaults: { duration: 0.75, ease: "power2.Out" },
 });
 
+tl.fromTo(".span-h3", { y: "-100%" }, { y: 0 });
+
 tl.fromTo(
   ".span-h1",
   { opacity: 0, x: "-100%" },
-  { opacity: 1, x: 0, duration: 1.5 }
+  { opacity: 1, x: 0, duration: 1.5 },
+  "<50%"
 );
 
 tl.fromTo(
   ".header__btn",
-  { y: "20%", opacity: 0 },
-  { y: 0, opacity: 1 },
+  { y: "50%", opacity: 0 },
+  { y: 0, opacity: 1, duration: 1.5, ease: "power4.out" },
   "<50%"
 );
--- up/app/app.js
+++ up/app/app.js
@@ -10,6 +10,8 @@ var tl = gsap.timeline({
   defaults: { duration: 0.75, ease: "power2.Out" }
 });
 
-tl.fromTo(".span-h1", { opacity: 0, x: "-100%" }, { opacity: 1, x: 0, duration: 1.5 });
+tl.fromTo(".span-h3", { y: "-100%" }, { y: 0 });
 
-tl.fromTo(".header__btn", { y: "20%", opacity: 0 }, { y: 0, opacity: 1 }, "<50%");
+tl.fromTo(".span-h1", { opacity: 0, x: "-100%" }, { opacity: 1, x: 0, duration: 1.5 }, "<50%");
+
+tl.fromTo(".header__btn", { y: "50%", opacity: 0 }, { y: 0, opacity: 1, duration: 1.5, ease: "power4.out" }, "<50%");
